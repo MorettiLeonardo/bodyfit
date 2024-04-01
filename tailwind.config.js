b0b497fd7443adab2365be3163624e970859ff4e
Leonardo Moretti <leonardonabarro2003@gmail.com>
2024-04-01 16:50:35 -0300

    initial config

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+export default {
+  mode: 'jit',
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false,
+  theme: {
+    extend: {}
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: []
+}
