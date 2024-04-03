86f001e2f32b26e1150a3badbc8176b4e11e41eb
Leonardo Moretti <leonardonabarro2003@gmail.com>
2024-04-03 14:18:01 -0300

    feat: create hero content and adjusting header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,7 +3,7 @@ import { ListCollapse, Plus, User } from 'lucide-react'
 import Logo from '../../assets/images/BodyFit.png'
 
 const Header = () => (
-  <header className="flex items-center justify-between bg-zinc-950 p-4 text-white font-medium">
+  <header className="w-full flex absolute items-center justify-between bg-transparent p-4 text-white font-medium">
     <img
       className="w-48 h-18 cursor-pointer"
       src={Logo}
@@ -12,50 +12,74 @@ const Header = () => (
     <nav>
       <ul className="flex items-center gap-4">
         <li>
-          <a className="cursor-pointer hover:text-red-600 transition" href="#">
+          <a
+            className="cursor-pointer hover:text-red-600 transition ease-linear"
+            href="#"
+          >
             Home
           </a>
         </li>
         <li>
-          <a className="cursor-pointer hover:text-red-600 transition" href="#">
+          <a
+            className="cursor-pointer hover:text-red-600 transition ease-linear"
+            href="#"
+          >
             Sobre
           </a>
         </li>
         <li>
-          <a className="cursor-pointer hover:text-red-600 transition" href="#">
+          <a
+            className="cursor-pointer hover:text-red-600 transition ease-linear"
+            href="#"
+          >
             Galeria
           </a>
         </li>
         <li>
-          <a className="cursor-pointer hover:text-red-600 transition" href="#">
+          <a
+            className="cursor-pointer hover:text-red-600 transition ease-linear"
+            href="#"
+          >
             Programação
           </a>
         </li>
         <li>
-          <a className="cursor-pointer hover:text-red-600 transition" href="#">
+          <a
+            className="cursor-pointer hover:text-red-600 transition ease-linear"
+            href="#"
+          >
             Blog
           </a>
         </li>
         <li>
-          <a className="cursor-pointer hover:text-red-600 transition" href="#">
+          <a
+            className="cursor-pointer hover:text-red-600 transition ease-linear"
+            href="#"
+          >
             Preços
           </a>
         </li>
         <li>
-          <a className="cursor-pointer hover:text-red-600 transition" href="#">
+          <a
+            className="cursor-pointer hover:text-red-600 transition ease-linear"
+            href="#"
+          >
             Aulas
           </a>
         </li>
         <li>
-          <a className="cursor-pointer hover:text-red-600 transition" href="#">
+          <a
+            className="cursor-pointer hover:text-red-600 transition ease-linear"
+            href="#"
+          >
             Contato
           </a>
         </li>
       </ul>
     </nav>
     <div className="flex gap-4 items-center">
-      <User className="cursor-pointer" />
-      <ListCollapse className="cursor-pointer" />
+      <User className="cursor-pointer hover:text-red-600 transition ease-linear" />
+      <ListCollapse className="cursor-pointer hover:text-red-600 transition ease-linear" />
       <button className="flex items-center justify-center border-solid border-2 border-zinc-500 rounded-sm p-2 gap-2">
         <Plus className="w-8 h-8 bg-red-600 rounded hover:rotate-[180deg] transition ease-linear" />
         <p>Junte-se a nós agora</p>
---
+++ src/components/HomeHero/index.tsx
@@ -0,0 +1,16 @@
+import { ArrowRight } from 'lucide-react'
+
+const HomeHero = () => (
+  <main className="hero-section h-screen bg-no-repeat object-cover">
+    <div className="top-96 right-52 absolute">
+      <h1 className="text-zinc-50 text-6xl max-w-xl uppercase leading-normal">
+        <span className="font-bold ">Faça seu corpo</span> fit & perfeito
+      </h1>
+      <button className="bg-white flex item-center p-4 gap-2 mt-4 font-bold uppercase transition ease-linear hover:text-zinc-50 hover:bg-zinc-900">
+        Nossas aulas <ArrowRight className="text-red-700" />
+      </button>
+    </div>
+  </main>
+)
+
+export default HomeHero
